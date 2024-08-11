e69cdd9c4a04ffcc3842cc56a0789ee03a79e088
Flea <shin2568@outlook.jp>
2024-08-11 18:21:48 +0900

    base

---
+++ EzTOLGet/EzTOLGet.cpp
@@ -0,0 +1,18 @@
+﻿#include "pch.h"
+#include "EzTOLGet.h"
+
+#include "bakkesmod\wrappers\GameEvent\TutorialWrapper.h"
+#include "bakkesmod\wrappers\GameObject\BallWrapper.h"
+#include "bakkesmod\wrappers\GameObject\CarWrapper.h"
+using namespace std;
+
+
+BAKKESMOD_PLUGIN(EzTOLGet, "BakkesModTest", "0.1", PLUGINTYPE_FREEPLAY)
+
+void EzTOLGet::onLoad() {
+	//load message
+	cvarManager->log(std::string(exports.pluginName) + std::string(" version: ") + std::string(exports.pluginVersion));
+}
+
+void EzTOLGet::onUnload() {
+}
---
+++ EzTOLGet/EzTOLGet.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+#include "bakkesmod/plugin/bakkesmodplugin.h"
+#pragma comment( lib, "pluginsdk.lib" )
+
+class EzTOLGet : public BakkesMod::Plugin::BakkesModPlugin
+{
+public:
+	void onLoad() override;
+	void onUnload() override;
+};
